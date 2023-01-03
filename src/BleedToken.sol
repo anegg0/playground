9c83dafb33df26af8fe0f18c553f39e5f3dc5bd9
anegg0 <anegg0@pop-os.localdomain>
2023-01-03 08:11:24 -0800

    Renamed contract from "bleed" to "bleedToken"

--- src/BleedToken.sol
+++ src/BleedToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "openzeppelin-contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-contracts/access/Ownable.sol";
 
-contract Bleed is ERC20, Ownable {
+contract BleedToken is ERC20, Ownable {
     constructor() ERC20("Bleed", "BLD") {
         _mint(msg.sender, 100 * 10 ** decimals());
     }
