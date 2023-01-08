92abed22621d78e4fb5e3e913daaa4ec8777c987
anegg0 <anegg0@pop-os.localdomain>
2023-01-08 10:06:34 -0800

    Functional test for throttledWallet.t.sol

--- test/throttledWallet.t.sol
+++ test/throttledWallet.t.sol
@@ -2,34 +2,40 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-
+import "forge-std/console.sol";
 import "../src/ThrottledWallet.sol";
 import "../src/BleedToken.sol";
 
 abstract contract HelperContract {
     BleedToken bleedtoken;
-
-    // constructor() ERC20("Bleed", "BLD") {
-    //     _mint(msg.sender, 100 * 10 ** decimals());
-    // }
 }
 
 contract ThrottledWalletTest is Test, HelperContract {
     ThrottledWallet throttledwallet;
 
-    // constructor() {
-    //     refillRate = 1000;
-    //     lastTransaction = 0;
-    // }
-
     function setUp() public {
         bleedtoken = new BleedToken();
-        throttledwallet = new ThrottledWallet();
+        throttledwallet = new ThrottledWallet(address(bleedtoken));
     }
 
-    function testDeposit() public {
-        address account = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
-        uint256 amount = 1;
-        assertEq(throttledwallet.deposit(account, amount), true);
+    function testTransfer() public {
+        address addressToTransferTo = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
+        assertEq(bleedtoken.transfer(addressToTransferTo, 15), true);
+    }
+
+    function testApprove() public {
+        address addressToApprove = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
+        assertEq(bleedtoken.approve(addressToApprove, 15), true);
+    }
+
+    function testTransferFrom() public {
+        address owner = 0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496;
+        address spender = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
+        address receiver = 0x90F79bf6EB2c4f870365E785982E1f101E93b906;
+        bleedtoken.transfer(owner, 50);
+        hoax(owner);
+        bleedtoken.approve(spender, 10);
+        hoax(spender);
+        assertEq(bleedtoken.transferFrom(owner, receiver, 2), true);
     }
 }
